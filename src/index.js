34bb4333cc429477010c41c6c672e4544181a928
daanniill <daniil.malakhov12@gmail.com>
2025-08-27 17:06:14 -0700

    styled the main home page and added dialogue like description, want to rework tho maybe make it the about page

--- src/index.js
+++ src/index.js
@@ -6,33 +6,37 @@ function createHomeContent() {
     // Get the existing content container
     const content = document.querySelector("#content");
 
+    const wrapper = document.createElement("div")
+    wrapper.setAttribute("class", "main")
+
+
     // Image
     const img = document.createElement("img");
     img.src = linkImage;
     img.alt = "link";
 
-    // Description wrapper
-    const description = document.createElement("div");
-    description.className = "description";
-
-    // Heading
-    const heading = document.createElement("h2");
-    heading.textContent = "Welcome to Triforce Tavern";
-
     // Info text
     const info = document.createElement("div");
     info.id = "info";
-    info.textContent = `Welcome to Triforce Tavern, a dining hall for heroes, wanderers, and royalty alike.
+
+    // Heading
+    const heading = document.createElement("h2");
+    heading.textContent = "Link";
+    
+    // description
+    const description = document.createElement('p')
+    description.id = "description"
+    description.textContent = `Welcome to Triforce Tavern, a dining hall for heroes, wanderers, and royalty alike.
         Inspired by the world of The Legend of Zelda, our menu brings together hearty meals from across Hyrule — Goron-sized roasts, fresh Zora-inspired seafood, and sweet Lon Lon Ranch desserts.
         Whether you’re gathering your party before a great quest or simply looking to restore your hearts after a long day, Triforce Tavern is the place where courage, wisdom, and power come together in every bite.`;
 
-    // Build structure
-    description.appendChild(heading);
-    description.appendChild(info);
+    info.appendChild(heading)
+    info.appendChild(description)
 
     // Append everything into #content
-    content.appendChild(img);
-    content.appendChild(description);
+    wrapper.appendChild(img);
+    wrapper.appendChild(info)
+    content.appendChild(wrapper);
 }
 
 createHomeContent()
